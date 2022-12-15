d1c3bb241b87549bdf03bfa6e891fafb111d83fe
ashura2428 <106125062+ashura2428@users.noreply.github.com>
2022-12-15 20:28:38 +0900

    Add files via upload

--- portfolios/RPG_battl_simulator/main.js
+++ portfolios/RPG_battl_simulator/main.js
@@ -15,55 +15,89 @@ let str = 2;
 let def = 1;
 let maxhp = 100;
 let hp = 100;
-let kougeki;
+let g = 0;
+let g_kakutoku = 0;
+let kougeki = 1;
 //敵の変数--------------------------------
+let encount1 = 100;     //最初のステージのエンカウント確立　敵の数 / encount で出す
 let str_teki = 0;
 let def_teki = 0;
-let hp_teki = 0
-let kougeki_teki;
-let teki_name
+let maqxhp_teki = 1;
+let hp_teki = 1;
+let kougeki_teki = 1;
+let teki_name = "";
+let teki_number = 1;
 //戦闘の変数--------------------------------
-let teki = 1 //敵の種類
-let sentou = 0 //戦闘中は1
-let turn = 0 //戦闘のターン
+let rnd; //エンカウントに使用のランダム
+let sentou = 0; //戦闘中は1
+let turn = 0; //戦闘のターン
+//アニメーションの変数
+function doAnimation(id, type){
+	const elem = $(id);
+	elem.addClass("animate__animated");
+	elem.addClass(type);
+	elem.on("animationend", ()=>{
+		elem.off("animationend");
+		elem.removeAttr("class");
+	});
+};
 
 
 //初期計算 & 初期化---------------------------------------------
 exp_tugi = Math.round(12 * (lv * 1.75 * lv / 1.71456));
 exp_hituyou += Math.round(exp_tugi);  //Math.roundは四捨五入
-$("#exp").text(exp_hituyou+" / "+exp);
+$("#exp").text(exp+" / "+exp_hituyou);
 $("#str").text(str);
 $("#def").text(def);
 
 
 //探索ボタン---------------------------------------------------
 	$("#btn1").click(()=>{
-		let rnd =Math.floor(Math.random() * (teki + 1 - 1)) +1;
+		$("#text2").text("");
+		rnd = Math.floor(Math.random() * (encount1 + 1 - 1)) +1;
 		console.log(rnd);
-		console.log(sentou);
 		if(sentou <= 0){
-		if(rnd >= teki){
-			$("#text1").text("スライムが現れた");
-			sentou = 1
-			str_teki = 2
-			def_teki = 1
-			hp_teki = 10
-			exp_kakutoku = 10
-			kougeki_teki = Math.round(5*(str_teki-=def)*(lv / 2.542));
-			if(kougeki_teki <= 0){kougeki_teki = 0};
-			teki_name = "スライム"
-			$("#hp_teki").text("HP "+10+" / "+hp_teki);
-			$("#teki").text(teki_name);
 
-		}else if(rnd >= teki -1){
-			$("#text1").text("ゴブリンが現れた");
-			sentou = 1
+		if(rnd < 50){
+			teki_name = "スライム";
+			teki_number = 1; 	
+			str_teki = 2;
+			def_teki = 1;
+			maxhp_teki = 10;
+			hp_teki = maxhp_teki;
+			exp_kakutoku = 10;
+			g_kakutoku = 2;
+			if(kougeki_teki <= 0){kougeki_teki = 1};
 
-		}else if(rnd >= teki -2){
-			$("#text1").text("こうもりが現れた");
-			sentou = 1
+		}else if(rnd < 87){
+			teki_name = "コウモリ";
+			teki_number = 3; 
+			str_teki = 4;
+			def_teki = 2;
+			maxhp_teki = 15;
+			hp_teki = maxhp_teki;
+			exp_kakutoku = 14;
+			g_kakutoku = 3;
+			if(kougeki_teki <= 0){kougeki_teki = 1};
 
-		}
+		}else if(rnd >= 87){
+			teki_name = "ゴブリン";
+			teki_number = 2; 
+			str_teki = 6;
+			def_teki = 3;
+			maxhp_teki = 20;
+			hp_teki = maxhp_teki;
+			exp_kakutoku = 23;
+			g_kakutoku = 5
+			if(kougeki_teki <= 0){kougeki_teki = 1};
+			
+		};
+		sentou = 2
+		$("#text1").text(teki_name+"が現れた");
+		$("#hp_teki").text("HP "+hp_teki+" / "+maxhp_teki);
+		$("#teki").html(`<img id="teki_image" src="./images/`+teki_number+`.png">`);
+		kougeki_teki = Math.round(5*(str_teki-=def)*(lv / 2.542));
+
 	}else if(sentou >= 1){
 		$("#text1").text("戦闘中は探索ができない！");
 	};
@@ -72,56 +106,81 @@ $("#def").text(def);
 //攻撃ボタン-------------------------------------------------
 	$("#btn2").click(()=>{
 		str = strmoto
-		kougeki = Math.round(5*(str-=def_teki)*(lv / 2.542));
-		if(sentou >= 1){
-			if(kougeki <= 0){kougeki= 0};
-			turn+=1
+		kougeki = Math.round(5*(str-=def_teki)*(lv/2.542));
+		if(sentou >= 2){
+			if(kougeki <= 0){kougeki = 1};
+			turn += 1
 			if(turn <= 1){
-			$("#text1").text(teki_name+`に`+kougeki+"ダメージ！");
-			$("#btn2").text("進む");
-			hp_teki -= Math.round(kougeki);
-			$("#hp_teki").text("HP "+10+" / "+hp_teki);
-		}else if(turn <= 2){
-			$("#text1").text(teki_name+"の攻撃"+kougeki_teki+"ダメージ！");
-			hp -= Math.round(kougeki_teki);
-			$("#hp").text(maxhp+" / "+hp);
-			$("#btn2").text("攻撃");
-			turn = 0
-			if(hp_teki <= 0){
-				str = strmoto
-				hp_teki = 0;
-				$("#text1").text(teki_name+"に勝利した！経験値を"+exp_kakutoku+"手に入れた！");
-				exp += exp_kakutoku;
-				$("#exp").text(exp_hituyou+"/"+exp);
-				$("#teki").text("");
-				$("#hp_teki").text("");
-				sentou = 0
-
-				//レベルの処理-----------------------------------------------------------
-				if (exp >= exp_hituyou){ // レベルアップ処理
-					lv += 1
-					exp_tugi *= 2
-					exp_tugi = 12 * (lv * 1.75 * lv / 1.71456);
-						if (exp_tugi >= 60000){
-							exp_tugi = 60000;
-						};	
-					exp_hituyou += Math.round(exp_tugi);
-					$("#exp").text(exp_hituyou+"/"+exp);
-					$("#lv").text("lv " + lv);
-					if(lv <=10){
-						maxhp += Math.round(25*(lv/2.845));
-						strmoto += 1*(lv/3.14);
-						str = strmoto
-						def += 1*(lv/3.14);
-					}
-					hp = maxhp;
-					$("#hp").text(Math.round(maxhp)+" / "+Math.round(hp));
-					$("#str").text(Math.round(str));
-					$("#def").text(Math.round(def));
-					console.log(str)
-				};
+				doAnimation("#teki_image", "animate__headShake");
+				$("#text1").text(teki_name+`に`+kougeki+"ダメージ！");
+				$("#btn2").text("進む");
+				hp_teki -= Math.round(kougeki);
+				$("#hp_teki").text("HP "+hp_teki+" / "+maxhp_teki);
+				if(hp_teki <= 0){
+					hp_teki = 0;
+					$("#hp_teki").text("HP "+hp_teki+" / "+maxhp_teki);
+				}
+			// 敵の攻撃---------------------------------------------------------------
+			}else if(turn <= 2){
+				doAnimation("#hp", "animate__headShake");
+				$("#text1").text(teki_name+"の攻撃"+kougeki_teki+"ダメージ！");
+				hp -= Math.round(kougeki_teki);
+				$("#hp").text(hp+" / "+maxhp);
+				$("#btn2").text("攻撃");
+				turn = 0		
 			};
 		};
-		};
+		// 敵を倒した処理-----------------------------------------------------
+		if(hp_teki <= 0){
+			if(sentou >= 1){turn += 1};
+			sentou = 1
+			if(turn >= 3){
+				$("#btn2").text("攻撃");
+				str = strmoto;
+				hp_teki = 1;
+				$("#text1").text(teki_name+"に勝利した！ 経験値を "+exp_kakutoku+" 手に入れた！");
+				$("#text2").text("お金を "+g_kakutoku+" 手に入れた！");
+				g += g_kakutoku;
+				exp += exp_kakutoku;
+				doAnimation("#exp", "animate__pulse");
+				$("#exp").text(exp+" / "+exp_hituyou);
+				$("#g").text(g);
+				$("#teki").text("");
+				$("#hp_teki").html("");
+				sentou = 0
+				turn = 0
+			};
+			// レベルの処理-----------------------------------------------------------
+			if (exp >= exp_hituyou){
+				lv += 1
+				doAnimation("#lv", "animate__heartBeat");
+				exp_tugi *= 2
+				exp_tugi = 12 * (lv * 1.75 * lv / 1.71456);
+					if (exp_tugi >= 60000){
+						exp_tugi = 60000;
+					};	
+				exp_hituyou += Math.round(exp_tugi);
+				$("#exp").text(exp+" / "+exp_hituyou);
+				$("#lv").text("Lv " + lv);
+				if(lv <=50){
+					maxhp += Math.round(25*(lv/2.845));
+					strmoto += 1*(lv/4.14);
+					str = strmoto;
+					def += 1*(lv/4.84);
+				}
+				hp = maxhp;
+				$("#hp").text(Math.round(hp)+" / "+Math.round(maxhp));
+				$("#str").text(Math.round(str));
+				$("#def").text(Math.round(def));
+				console.log(str)
+			}};	
 	});
+
+	// セーブデータ機能----------------------------------------------------
+	// 保存
+	function save() {
+        var mydata = document.getElementById("mydata_in").value;
+        console.log(`mydata_in = ${mydata_in}`);
+        localStorage.setItem('mydata', mydata);
+      };
 });
